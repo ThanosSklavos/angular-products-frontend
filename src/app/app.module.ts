2fba70e879d641fc4a3c6ae28025dc8104383543
Thanos Sklavos <thanos.sklavos@outlook.com>
2023-04-15 13:56:35 +0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
+import { ProductInsertComponent } from './product-insert/product-insert.component';
 
 const routes: Routes = [
-  { path: 'user', loadChildren: () => import('./user/user.module').then((m) => m.UserModule)},
-  { path: 'product', loadChildren: () => import('./products/products.module').then((m) => m.ProductsModule)},
+  { path: 'user',
+   loadChildren: () => import('./user/user.module').then((m) => m.UserModule)},
+  { path: 'product', 
+   loadChildren: () => import('./products/products.module').then((m) => m.ProductsModule)},
   { path: '', component: WelcomeComponent },
   { path: '**', component: PageNotFoundComponent }
 ]
@@ -19,7 +22,8 @@ const routes: Routes = [
     AppComponent,
     WelcomeComponent,
     PageNotFoundComponent,
-    DropdownComponent
+    DropdownComponent,
+    ProductInsertComponent
   ],
   imports: [
     BrowserModule, RouterModule.forRoot(routes)
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductInsertComponent } from './product-insert/product-insert.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductInsertComponent,
+    ProductListComponent
+  ],
   imports: [
     CommonModule
   ]
